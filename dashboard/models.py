25bc1329f032a66f6045858f6d3a22d053f84c28
KOU <elbehi.koutheir@gmail.com>
2020-05-11 18:09:30 +0100

    Welcome

---
+++ dashboard/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+    is_club = models.BooleanField(default=False)
+    is_dean = models.BooleanField(default=False)
+
+
+# class Dean(models.Model):
+class Request(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Club(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    requests = models.ManyToManyField(Request, related_name='requests')
