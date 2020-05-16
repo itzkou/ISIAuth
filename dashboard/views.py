c8e49a51c56b69ca1af5832c9f892bf5c02259af
KOU <elbehi.koutheir@gmail.com>
2020-05-16 16:36:52 +0100

    login logout signup is set

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import TemplateView, CreateView
@@ -26,8 +27,28 @@ class ClubSignUpView(CreateView):
     form_class = ClubSignUpForm
     template_name = 'registration/signup_form.html'
 
+    # You will probably be overriding this method most often to add things to display in your templates.
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'club'
+        return super().get_context_data(**kwargs)
+
+    ''' This method is called when correct data is entered into the form and the form has been successfully validated without any errors. You can handle post-success logic here like send a notification email to the user, redirect to a thank you page etc.'''
+
+    def form_valid(self, form):
+        user = form.save()  # saving the user that submitted the form
+        login(self.request, user)  # login into the user account
+        return redirect('home')  # redirecting to home URL (home ->HomeView)
+
 
 class DeanSignUpView(CreateView):
     model = User
     form_class = DeanSignUpForm
     template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'staff'
+        return super().get_context_data(**kwargs)
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
